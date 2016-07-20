9dc68d80529a2e58dd77037965d4a885cb77c3e6
bappa <bappa2du@gmail.com>
2016-07-20 14:49:01 +0600

    Update

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,27 +6,35 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-        // default image driver is gd
-        // $this->image = new ImageManager(['driver' => 'imagick']);
-        // $this->paginate();
-    }
-
-    /*public function paginate()
-    {
-    $currentPage = isset($_GET['page']) ? (int) $_GET['page'] : 1;
-    Paginator::currentPageResolver(function () use ($currentPage) {
-    return $currentPage;
-    });
-    }*/
 
     public function index()
     {
+        // $this->paginate();
         // $user = UserModel::paginate();
+        // $user = UserModel::find($id);
+        // $user = UserModel::where('name',LIKE,'%Alex%');
         // dump($user);
         view('welcome');
+    }
+
+    public function upload_image($image_file, $image_name)
+    {
+        // default image driver is gd
+        $image = new ImageManager(['driver' => 'imagick']);
+        $image->make($image_file)
+            ->resize(200, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })
+            ->crop(200, 200)
+            ->save('upload/path/' . $image_name);
+    }
+
+    public function paginate()
+    {
+        $currentPage = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+        Paginator::currentPageResolver(function () use ($currentPage) {
+            return $currentPage;
+        });
     }
 
 }
