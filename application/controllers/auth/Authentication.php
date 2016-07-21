536f24eaa130cfd5b3a80863ff4959185d5f5085
bappa <bappa2du@gmail.com>
2016-07-21 18:02:22 +0600

    Updating

--- application/controllers/auth/Authentication.php
+++ application/controllers/auth/Authentication.php
@@ -1,10 +1,21 @@
 <?php
 
+use Cartalyst\Sentinel\Native\Facades\Sentinel;
+
 class Authentication extends CI_Controller
 {
     public function login()
     {
         view('auth/login');
+    }
+
+    public function attempt()
+    {
+        $r = Sentinel::register([
+            'email'    => 'test@example.com',
+            'password' => 'foobar',
+        ]);
+        dump($r);
     }
 
     public function logout()
